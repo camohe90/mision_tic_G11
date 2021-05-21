e65a9249a0e2d5bd64635c2f309fadbb24d4f854
Camilo <camohe90@gmail.com>
2021-05-21 11:24:42 -0500

    While en python

---
+++ s12/12.6_while_suma.py
@@ -0,0 +1,8 @@
+suma = 0
+mensaje = ("Ingrese los numero que desea sumar")
+
+while suma <=100:
+    numero = int(input(mensaje))
+    suma = suma + numero
+
+print("La suma de los numero ingresados es", suma)
